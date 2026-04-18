cb8deeea98ca7ea49803502d33b0c8c0726d4d6d
Aaron Madlon-Kay <aaron@madlon-kay.com>
2026-04-18 16:53:36 +0900

    dart create

---
+++ bin/org_social_mastodon_bridge.dart
@@ -0,0 +1,5 @@
+import 'package:org_social_mastodon_bridge/org_social_mastodon_bridge.dart' as org_social_mastodon_bridge;
+
+void main(List<String> arguments) {
+  print('Hello world: ${org_social_mastodon_bridge.calculate()}!');
+}
---
+++ test/org_social_mastodon_bridge_test.dart
@@ -0,0 +1,8 @@
+import 'package:org_social_mastodon_bridge/org_social_mastodon_bridge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
